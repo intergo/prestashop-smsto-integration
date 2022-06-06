9eb9712d689e8b8722309ab56a2853949cce1f16
Panayiotis Halouvas <phalouvas@kainotomo.com>
2022-06-06 11:25:37 +0300

    fixed description

--- smsto.php
+++ smsto.php
@@ -21,9 +21,7 @@ class SMSto extends Module
         parent::__construct();
 
         $this->displayName = $this->l('SMSto - Integration Module');
-        $this->description = $this->l('The SMSto SMS Integration is an integration with the OpenCart e-commerce platform. This Integration enables OpenCart store admins to configure automated SMS notifications to the administrator and customers for important order status updates, and also allows sending bulk SMS messages to customers. The Integration is free, but a SMSto account is required to send messages. Signup with our service is free as well, and you pay only for the SMS messages. The Integration offers great flexibility, in sending individual SMS or bulk SMS messages to various groups.
-        
-        No contracts, no commitments, pay only for what you use. In case of high volume SMS API usage, our sales team is prepared to give out additional discounts.');
+        $this->description = $this->l('The SMSto SMS Integration is an integration with the PrestaShop e-commerce platform. This Integration enables PrestaShop store admins to configure automated SMS notifications to the administrator and customers for important order status updates, and also allows sending bulk SMS messages to customers. The Integration is free, but a SMSto account is required to send messages. Signup with our service is free as well, and you pay only for the SMS messages. The Integration offers great flexibility, in sending individual SMS or bulk SMS messages to various groups.');
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 
